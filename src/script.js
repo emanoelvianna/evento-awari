a18b4d48bb2b9b2d33ab0641ca422c9ce3cda2d2
Emanoel Vianna <vianna.emanoel@gmail.com>
2022-09-14 17:25:29 -0300

    main - ajustes sobre o projeto

--- src/script.js
+++ src/script.js
@@ -1,18 +1,19 @@
-// Get the modal
+// buscando o elemento que representa o furmulário
 var modal = document.getElementById("formulario");
 
-// Get the button that opens the modal
-var btn = document.getElementById("reservar");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-var enviar = document.getElementById("enviar");
+// buscando o elemento que representa o botão de revervar
+var button = document.getElementById("reservar");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
+// criando o comportamento para mostrar o formulário ao clicar no botão
+button.onclick = function () {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// buscando o elemento que representa o botão de fechar o formulário
+var span = document.getElementsByClassName("close")[0];
+var enviar = document.getElementById("enviar");
+
+// criando o comportamento para fechar o formulário
 span.onclick = function () {
     modal.style.display = "none";
 }
@@ -21,7 +22,7 @@ enviar.onclick = function () {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// criando o comportamento para fechar ao clicar fora do formulário
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
